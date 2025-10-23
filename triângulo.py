ccafdbee1f0fe93987fb1bd34bad0e922ac53223
Caio Felix <caiofelixmaia@gmail.com>
2025-10-23 12:02:25 -0300

    Add files via upload

---
+++ triângulo.py
@@ -0,0 +1,11 @@
+# Faça uma função que recebe a base e a altura e retorna a área do triângulo
+
+def funcao():
+    
+    b = int(input("Digite a base: "))
+    a = int(input("Digite a altura: "))
+    area = b * a/2 
+    return area
+# print(area)
+resultado = funcao()
+print(f"O resultado é {resultado}")
